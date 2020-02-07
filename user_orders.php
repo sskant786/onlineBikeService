544d2f4444b7ae2be84ee2591a46b98bbfb0f2d4
parvati2 <parvati2.paru@gmail.com>
2020-02-08 00:00:24 +0530

    added php code in user_order page

--- user_orders.php
+++ user_orders.php
@@ -1,3 +1,44 @@
 <?php
+session_start();
+if($_SESSION['name'] == "") {
+header('location:index.php');
+}else {
+include("menu_bar_logout.php");
+}
 include('db_connection/connection.php');
+$query = "SELECT * FROM orders";
+$data = mysql_query($conn, $query);
+echo "conn11 -";
 ?>
+<html>
+<head>
+<title>orders</title>
+</head>
+  <body>
+    	<div class="center-content">
+	<table align="center">
+	<tr>
+	  <th colspan="4"><h2 style="color:green">Your Order</h2></th>
+	</tr>
+	<tr>
+	  <th>Order Id</th>
+	  <th>Type</th>
+	  <th>Status</th>
+	  <th>Discription</th>
+	 </tr>
+	 <?php
+	  echo "$query - $data";
+	  while($row = mysqli_fetch_assoc($data)){
+		  echo "in while loop";
+	   echo "<tr>
+		 <td>".$row['order_id']."</td>
+		 <td>".$row['type']."</td>
+		 <td>".$row['status']."</td>
+		 <td>".$row['discription']."</td>
+	   </tr>";
+	  }
+	 ?>
+	 </table>
+    </div>	
+  </body>
+<html>
